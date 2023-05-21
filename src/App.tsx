97c3b7933888e09a64a98bc17acc0f882e88ca2b
devjean <jeancarloleall@gmail.com>
2023-05-20 23:30:59 -0300

    animated-form

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <main>
+      <header className="header-container">
+        <img src="./logo-image.svg" />
+        <nav className="menu-options">
+          <a>What we offer</a>
+          <a>How it works</a>
+          <a>Portfolio</a>
+        </nav>
+        <button>Contact us</button>
+      </header>
+    </main>
+  );
+}
+
+export default App;
